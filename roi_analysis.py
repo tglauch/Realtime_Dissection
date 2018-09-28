e3ce465bc77323786efa3437e0cae2d33d3fa170
Theo Glauch <theo.glauch@tum.de>
2018-09-28 11:28:57 +0200

    initial commit

---
+++ roi_analysis.py
@@ -0,0 +1,79 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+import sys
+sys.path.append('/scratch9/tglauch/Fermi_Tools/get_fermi_data')
+sys.path.append('/scratch9/tglauch/realtime_service/python/lib/python2.7/site-packages')
+from get_fermi_data import get_data
+import subprocess
+import argparse
+import os
+from roi_functions import get_sources
+import shutil
+import datetime
+
+
+def parseArguments():
+    """Parse the command line arguments
+    Returns:
+    args : Dictionary containing the command line arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ra",
+        help="right ascension",
+        type=float, required=True)
+    parser.add_argument(
+        "--dec",
+        help="declination",
+        type=float, required=True)
+    args = parser.parse_args()
+    return args.__dict__
+
+
+def generate_src_xml(name, ra, dec, xml_path):
+    with open('./src_xml_draft.xml', 'r') as f:
+        xml_str = (f.read()).format(name=name, ra=ra, dec=dec)
+    with open(xml_path, 'w+') as f:
+        f.write(xml_str)
+    return
+
+
+dtime = datetime.datetime.now()
+ev_str = 'IC{}{:02d}{}'.format(str(dtime.year)[-2:], dtime.month, dtime.day)
+bpath = '/scratch9/tglauch/realtime_service/output/{}'.format(ev_str)
+this_path = os.path.dirname(os.path.abspath(__file__))
+fermi_data = os.path.join(bpath, 'fermi_data')
+
+
+args = parseArguments()
+cmd = [os.path.realpath('/scratch9/tglauch/VOU_Blazars/bin/vou-blazars'),
+       str(args['ra']), str(args['dec']), str(120)]
+vou_out = os.path.join(bpath, 'vou_blazar' )
+if not os.path.exists(vou_out):
+    os.makedirs(vou_out)
+os.chdir(vou_out)
+subprocess.call(cmd)
+os.chdir(this_path)
+print('Get Sources....')
+src_dict = get_sources(args['ra'], args['dec'])
+#get_data(args['ra'], args['dec'], emin=2000,
+#         dt=168, out_dir=fermi_data)  # dt hardcoded!!!!
+for i, src in enumerate(src_dict['name']):
+    src = src.replace(' ', '_')
+    bpath = os.path.join(bpath, src)
+    if os.path.exists(bpath):
+        shutil.rmtree(bpath)
+    os.makedirs(bpath)
+    args = '--target_src {} --free_radius 2 --data_path {} --use_3FGL --outfolder {} '.format(src, fermi_data, bpath)
+    if '3FGL' not in src:
+        xml_path = os.path.join(bpath, 'add_source.xml')
+        generate_src_xml(src, src_dict['ra'][i], src_dict['dec'][i], xml_path)
+        args += '--xml_path {}'.format(xml_path)
+
+    with open('./slurm_draft.xml', 'r') as f:
+        submit = (f.read()).format(bpath=bpath, args=args)
+    submitfile = os.path.join(bpath, 'submit.sub')
+    with open(submitfile, "w+") as file:
+        file.write(submit)
+ #   os.system("sbatch {}".format(submitfile))
---
+++ roi_functions.py
@@ -0,0 +1,94 @@
+import numpy as np
+from astropy.coordinates import SkyCoord
+import os
+import collections
+
+fields = ['name', 'ra', 'dec', 'alt_name']
+
+files = collections.OrderedDict(
+    {'3fgl': {'file': './results/3fgl.1.csv',
+              'keys': ['name', 'ra', 'dec']},
+     '3fhl': {'file': './results/3fhl.1.csv',
+              'keys': ['name', 'ra', 'dec']},
+     '5bzcat': {'file': './results/5bzcat.1.csv',
+                'keys': ['Name', 'RAJ2000', 'DEJ2000']},
+     '3hsp': {'file': './results/3hsp.1.csv',
+              'keys': ['Name', 'ra', 'dec']},
+     'fermi8yr': {'file': './results/fermi8yr.1.csv',
+                  'keys': ['Source_Name', 'RAJ2000', 'DEJ2000']},
+     'crates': {'file': './results/crates.1.csv',
+                'keys': ['name', 'ra', 'dec']}})
+
+
+def GreatCircleDistance(ra_1, dec_1, ra_2, dec_2):
+    '''Compute the great circle distance between two events'''
+    '''All coordinates must be given in radians'''
+    delta_dec = np.abs(dec_1 - dec_2)
+    delta_ra = np.abs(ra_1 - ra_2)
+    x = (np.sin(delta_dec / 2.))**2. + np.cos(dec_1) *\
+        np.cos(dec_2) * (np.sin(delta_ra / 2.))**2.
+    return 2. * np.arcsin(np.sqrt(x))
+
+
+def get_sources(ra, dec):
+    src_dict = {}
+    for key in fields:
+        src_dict[key] = []
+    for key in files.keys():
+        if not os.path.exists(files[key]['file']):
+            continue
+        temp = np.genfromtxt(files[key]['file'], dtype=None,
+                             names=True, delimiter=',')
+        if key == '5bzcat':
+            for src in temp:
+                ra_split = src[1].split(' ')
+                dec_split = src[2].split(' ')
+                c = SkyCoord('{}h{}m{}s'.format(ra_split[0],
+                                                ra_split[1],
+                                                ra_split[2]),
+                             '{}d{}m{}s'.format(dec_split[0],
+                                                dec_split[1],
+                                                dec_split[2]),
+                             frame='icrs')
+                src[1] = c.ra.degree
+                src[2] = c.dec.degree
+            dtype = [('Name', 'S15'), ('RAJ2000', np.float32),
+                     ('DEJ2000', '<f8')]
+            temp = temp.astype(dtype)
+        for i in range(len(files[key]['keys'])):
+            src_dict[fields[i]].extend(temp[files[key]['keys'][i]])
+
+    i = 0
+    while i < len(src_dict['dec']):
+        src_dict['alt_name'].append([])
+        ang_diff = [np.degrees(
+                    GreatCircleDistance(np.radians(src_dict['ra'][i]),
+                                        np.radians(src_dict['dec'][i]),
+                                        np.radians(src_dict['ra'][j]),
+                                        np.radians(src_dict['dec'][j])))
+                    for j in range(i + 1, len(src_dict['dec']))]
+        to_delete = []
+        for j, diff in enumerate(ang_diff):
+            if diff < 0.1:
+                if not src_dict['name'][i + j + 1] in src_dict['alt_name'][i] \
+                    and src_dict['name'][i + j + 1] != src_dict['name'][i]:
+                        src_dict['alt_name'][i].append(src_dict['name'][i + j + 1])
+                to_delete.append(i + j + 1)
+        for k, j in enumerate(to_delete):
+            del(src_dict['name'][j - k])
+            del(src_dict['ra'][j - k])
+            del(src_dict['dec'][j - k])
+        i += 1
+
+    fmt_str = '\n\n {} \n ra: {:.2f}deg \n dec: {:.2f}deg \n distance: {:.2f}deg \n alt names: {}'
+    for i in range(len(src_dict['name'])):
+        gcd = GreatCircleDistance(np.radians(src_dict['ra'][i]),
+                                  np.radians(src_dict['dec'][i]),
+                                  np.radians(ra),
+                                  np.radians(dec))
+        print(fmt_str.format(src_dict['name'][i],
+                             src_dict['ra'][i],
+                             src_dict['dec'][i],
+                             np.degrees(gcd),
+                             src_dict['alt_name'][i]))
+    return src_dict
