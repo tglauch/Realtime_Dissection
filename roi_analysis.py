7c4744a74cab042cc3faa8a83875025bf6e086d1
tglauch <theo.glauch@tum.de>
2018-10-01 00:54:02 +0300

    prepare for light curves

--- roi_analysis.py
+++ roi_analysis.py
@@ -4,7 +4,9 @@
 import sys
 sys.path.append('/scratch9/tglauch/Fermi_Tools/get_fermi_data')
 sys.path.append('/scratch9/tglauch/realtime_service/python/lib/python2.7/site-packages')
+sys.path.append('/scratch9/tglauch/Fermi_Tools/SED')
 from get_fermi_data import get_data
+from run_SED_fit import MET_to_MJD
 import subprocess
 import argparse
 import os
@@ -31,6 +33,29 @@ def parseArguments():
     return args.__dict__
 
 
+def submit_fit(args, bpath, src_dict, which, trange=''):
+    if trange is '':
+        opath = os.path.join(bpath, 'all_year')
+        args += ' --outfolder {} '.format(opath)
+    else:
+        opath = os.path.join(
+            bpath, 'lightcurve/{}_{}'.format(trange[0], trange[1]))
+        args += ' --time_range {} {} '.format(trange[0], trange[1])
+        args += ' --outfolder {} '.format(opath)
+    os.makedirs(opath)
+    if '3FGL' not in src:
+        xml_path = os.path.join(opath, 'add_source.xml')
+        generate_src_xml(src_dict['name'][which], src_dict['ra'][which],
+                         src_dict['dec'][which], xml_path)
+        args += '--xml_path {}'.format(xml_path)
+    with open('./slurm_draft.xml', 'r') as f:
+        submit = (f.read()).format(bpath=opath, args=args)
+    submitfile = os.path.join(opath, 'submit.sub')
+    with open(submitfile, "w+") as file:
+        file.write(submit)
+    os.system("sbatch {}".format(submitfile))
+
+
 def generate_src_xml(name, ra, dec, xml_path):
     with open('./src_xml_draft.xml', 'r') as f:
         xml_str = (f.read()).format(name=name, ra=ra, dec=dec)
@@ -49,7 +74,7 @@ fermi_data = os.path.join(bpath, 'fermi_data')
 args = parseArguments()
 cmd = [os.path.realpath('/scratch9/tglauch/VOU_Blazars/bin/vou-blazars'),
        str(args['ra']), str(args['dec']), str(120)]
-vou_out = os.path.join(bpath, 'vou_blazar' )
+vou_out = os.path.join(bpath, 'vou_blazar')
 if not os.path.exists(vou_out):
     os.makedirs(vou_out)
 os.chdir(vou_out)
@@ -57,24 +82,17 @@ subprocess.call(cmd)
 print('Get Sources....')
 src_dict = get_sources(args['ra'], args['dec'])
 os.chdir(this_path)
-get_data(args['ra'], args['dec'], emin=2000,
-         dt=364, out_dir=fermi_data)  # dt hardcoded!!!!
+MET = get_data(args['ra'], args['dec'], emin=2000,
+               dt=364, out_dir=fermi_data)  # dt hardcoded!!!!
+MJD = [MET_to_MJD(i) for i in MET]
 for i, src in enumerate(src_dict['name']):
     bpath_src = os.path.join(bpath, src.replace(' ', '_'))
     print bpath_src
     if os.path.exists(bpath_src):
         shutil.rmtree(bpath_src)
-    os.makedirs(bpath_src)
-    args = '--target_src {} --free_radius 2 --data_path {} --use_3FGL --outfolder {} '
-    args.format(src.replace(' ', '_'), fermi_data, os.path.join(bpath_src, 'all_time'))
-    if '3FGL' not in src:
-        xml_path = os.path.join(bpath_src, 'add_source.xml')
-        generate_src_xml(src, src_dict['ra'][i], src_dict['dec'][i], xml_path)
-        args += '--xml_path {}'.format(xml_path)
-
-    with open('./slurm_draft.xml', 'r') as f:
-        submit = (f.read()).format(bpath=bpath_src, args=args)
-    submitfile = os.path.join(bpath_src, 'submit.sub')
-    with open(submitfile, "w+") as file:
-        file.write(submit)
-    os.system("sbatch {}".format(submitfile))
+    args = '--target_src {} --free_radius 2 --data_path {} --use_3FGL '
+    args.format(src.replace(' ', '_'), fermi_data)
+    time_windows = [[k, k+28.] for k in np.arange(MJD[0], MJD[1], 28)]
+    time_windows.append('')
+    for t_window in time_windows:
+        submit_fit(args, bpath_src, src_dict, i, trange=time_windows)
