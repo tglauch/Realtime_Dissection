278787cfb5005d1c593711685d23eebda601e2b4
Theo Glauch <theo.glauch@tum.de>
2019-08-11 18:38:53 +0200

    automatically start when gcn comes in

---
+++ wait_for_gcn.py
@@ -0,0 +1,30 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import os
+
+def check_for_new_gcn():
+    print(datetime.datetime.now())
+    with open('./gcns.txt', 'r') as f:
+        gcns = f.read()
+    all_gcn = gcns.split('\n')
+    url = 'https://gcn.gsfc.nasa.gov/amon_icecube_gold_bronze_events.html'
+    html= requests.get(url).text.encode('utf8')
+    table = html.split('<!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX>')[1].split('<!--')[0]
+    parsed_html = BeautifulSoup(table, "html5lib")
+    for a in parsed_html.find_all('a', href=True):
+        full_href  =  'https://gcn.gsfc.nasa.gov/' + a['href']
+        if not full_href in all_gcn:
+            print('NEW GCN')
+            print(full_href)
+            all_gcn.append(full_href)
+            all_gcn = [i for i in all_gcn if i != '']
+            os.system('screen -d -m -S newevent bash -c \'python roi_analysis.py --gcn {}\''.format(full_href))
+    with open('./gcns.txt', 'w+') as f:
+        f.write('\n'.join(all_gcn))
+    return
+
+while True:
+    time.sleep(5)
+    check_for_new_gcn()
